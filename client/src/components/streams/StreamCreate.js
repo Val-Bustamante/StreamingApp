ad9c5ef552924942d3f535e8a06017dcbd224b38
Valentina Bustamante <valentinabustamante@Mobiledevs-MacBook-Pro.local>
2020-02-13 11:44:32 -0500

    Added redux form

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,13 +1,21 @@
 // /streams/new
-
+//Field is a componnet, reduxForm is a function
+import {Field, reduxForm} from 'redux-Form'
 import React from 'react'
 
-const StreamCreate =() => {
-    return (
-        <div>
-            StreamCreate
+//class based component because we will use helper components
+class StreamCreate extends React.Component {
+    render() {
+        console.log(this.props)
+        return (
+            <div>
+                StreamCreate
         </div>
-    )
+        )
+    }
 }
 
-export default StreamCreate
+//redux-form function acts the same way as connect, it will reach into action creator and retrive the info from the form
+export default reduxForm({
+    form: 'createStream'
+})(StreamCreate);
