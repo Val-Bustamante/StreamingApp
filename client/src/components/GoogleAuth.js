6edca46cd0e06b660291471de5197ac7d7529a0e
Valentina Bustamante <valentinabustamante@Mobiledevs-MacBook-Pro.local>
2020-02-13 10:12:57 -0500

    SignIn/Out button finished

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -2,33 +2,59 @@ import React from "react";
 
 class GoogleAuth extends React.Component {
 
-    state={isSignedIn: null}
+    state = { isSignedIn: null }
 
     //initializes OAuth library
     componentDidMount() {
-        window.gapi.load('client:auth2', ()=>{
+        window.gapi.load('client:auth2', () => {
             //init returns a promise after the client library has initialized
             window.gapi.client.init({
                 clientId: '749911539093-uu5rsf1g0v281emfcjo0blo7ud1q2ug4.apps.googleusercontent.com',
                 scope: 'email'
-            }).then(()=> {
+            }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
                 //now update component level state
-                this.setState({isSignedIn: this.auth.isSignedIn.get()})
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+                this.auth.isSignedIn.listen(this.onAuthChange)
             })
         })
     }
 
-    renderAuthButton(){
-        if(this.state.isSignedIn === null){
-            return <div>I don't know if we are signed in</div>
-        }else if(this.state.isSignedIn){
-            return <div>I am signed in!</div>
-        }else{
-            return <div>I am not signed in!</div>
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+    }
+
+    //arrow function since its a callback
+    onSignIn = () => {
+        this.auth.signIn()
+    }
+
+    onSignOut = () => {
+        this.auth.signOut()
+    }
+
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return null
+        } else if (this.state.isSignedIn) {
+            return <button
+                className="ui red google button"
+                onClick={this.onSignOut}>
+                <i className="google icon">
+                    Sign Out
+                </i>
+            </button>
+        } else {
+            return <button
+                className="ui red google button"
+                onClick={this.onSignIn}>
+                <i className="google icon">
+                    Sign In
+                </i>
+            </button>
         }
     }
-    
+
     render() {
         return (
             <div>
